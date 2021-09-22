905708dce1cefae3c14bf1215b133b7176ebe65d
siegfried <81885507+Fernando0313@users.noreply.github.com>
2021-09-22 17:43:02 -0500

    chore: modifique el workflow
    en este pr agregue lo que seria el correcto funcionamiento del workflow

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from controllers.actividades import ActividadesController
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(ActividadesController,'/actividades')
+# RUTAS
+api.add_resource(ActividadesController, '/actividades')
+
 print('yo soy un cambio')
-print('aea')
 
 if __name__ == '__main__':
     app.run(debug=True)
